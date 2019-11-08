1d8fd6d7807b1be263031aa27dfbd4be6b63f929
Joel Delatte <joeldelatte@gmail.com>
2019-11-07 21:58:29 -0600

    adds full html, some styling, and functional local storage saving

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # day-planner
 a simple calendar application that allows the user to save events for each hour of the day. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
+
+
+
+
+# Pseudo Code
+
+1. save button onclick function adding contents of textarea to local storage; use for loop to apply event handler function to each save button using a class selector 
+2. function to add saved contents back into the textarea onload of each textarea using for loop
+3. dynamically add attrubute/class of past, present or future that applies styling to each hour row based on its time relative to currenttime/moment() using is adding methods in moment api; use conditionals to compare current to fixed hour of each row.
+4. 
---
+++ script.js
@@ -0,0 +1,52 @@
+var dayDisplay = $("#header-date");
+var timeDisplay = $("#header-time");
+var save  = $(".save-button");
+var hour1   = $("#input-1");
+var hour2   = $("#input-2");
+var hour3   = $("#input-3");
+var hour4   = $("#input-4");
+var hour5   = $("#input-5");
+var hour6   = $("#input-6");
+var hour7   = $("#input-7");
+var hour8   = $("#input-8");
+var hour9   = $("#input-9");
+var  key1 = "hour1"
+var  key2 = "hour2"
+var  key3 = "hour3"
+var  key4 = "hour4"
+var  key5 = "hour5"
+var  key6 = "hour6"
+var  key7 = "hour7"
+var  key8 = "hour8"
+var  key9 = "hour9"
+var currentTime = moment();
+
+var hours = [hour1, hour2, hour3, hour4, hour5, hour6, hour7, hour8, hour9];
+var keys = [key1, key2, key3, key4, key5, key6, key7, key8, key9]
+
+dayDisplay.text(currentTime.format("dddd, MMM, Do"));
+timeDisplay.text(currentTime.format("h:mm a"))
+
+// add a for loop for all the save buttons?
+save.on("click", function() {
+    for (i = 0; i < keys.length; i++) {
+    localStorage.setItem(keys[i], hours[i].val());
+    console.log(keys[i])
+    } 
+})
+
+function loadSaved() {
+    for (i = 0; i < hours.length; i++) {
+    var hourPrint = window.localStorage.getItem(keys[i]);
+    hours[i].text(hourPrint);
+    console.log(hourPrint);
+    }
+}
+
+var timeStamps = [];
+// for (i = 0; i > )
+if (moment().format("H") > ) {
+$("<textarea>").attr("class", "past");
+}
+
+loadSaved()
